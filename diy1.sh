75d173534ee7539976beb2e4d8ab95c7a008d7e7
cddcx <84568016+cddcx@users.noreply.github.com>
2021-06-13 16:28:48 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -7,6 +7,14 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-#安装smartdns插件
+# 科学上网插件
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+#git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+
+# godproxy去广告插件
+#git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
+
+# smartdns插件
 git clone https://github.com/pymumu/smartdns.git package/lean/smartdn
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
