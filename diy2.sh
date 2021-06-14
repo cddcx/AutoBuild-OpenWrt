8de0cde9fa3f02c2306493ba540da8dc02336423
cddcx <84568016+cddcx@users.noreply.github.com>
2021-06-14 14:11:06 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -9,12 +9,12 @@
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 删除文件夹
-rm -rf package/lean/adbyby
-rm -rf package/lean/luci-app-adbyby-plus
-rm -rf package/lean/luci-app-unblockmusic
-rm -rf package/lean/UnblockNeteaseMusic
-rm -rf package/lean/UnblockNeteaseMusicGo
-rm -rf feeds/packages/net/smartdns
+#rm -rf package/lean/adbyby
+#rm -rf package/lean/luci-app-adbyby-plus
+#rm -rf package/lean/luci-app-unblockmusic
+#rm -rf package/lean/UnblockNeteaseMusic
+$rm -rf package/lean/UnblockNeteaseMusicGo
+$rm -rf feeds/packages/net/smartdns
 
 # 修改默认主题
 #sed -i 's/bootstrap/argon-dark-mod/' feeds/luci/collections/luci/Makefile
