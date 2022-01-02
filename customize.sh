75180c641eeaecbed2966d729764232bf501376c
cddcx <84568016+cddcx@users.noreply.github.com>
2022-01-02 13:41:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,13 +8,13 @@
 #Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# 删除文件夹
-#rm -rf package/lean/adbyby
-#rm -rf package/lean/luci-app-adbyby-plus
-#rm -rf package/lean/luci-app-unblockmusic
-#rm -rf package/lean/UnblockNeteaseMusic
-#rm -rf package/lean/UnblockNeteaseMusicGo
-#rm -rf feeds/packages/net/smartdns
+# 修改内核 5.4改为5.10
+sed -i 's/PATCHVER:=5.10/PATCHVER:=5.15/g' target/linux/x86/Makefile
+
+# 软件中心istore
+svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
+rm -rf package/luci-app-store/.svn
+sed -i 's/("iStore"), 31/("应用商店"), 61/g' package/luci-app-store/luasrc/controller/store.lua
 
 # 修改默认主题
 #sed -i 's/bootstrap/argon-dark-mod/' feeds/luci/collections/luci/Makefile
