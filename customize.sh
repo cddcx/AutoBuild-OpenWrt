eab2ca96f628e70f6cf89db08aac8c826f3e32e5
cddcx <84568016+cddcx@users.noreply.github.com>
2022-01-03 13:14:04 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,8 +8,8 @@
 #Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# 修改内核 5.4改为5.10
-sed -i 's/PATCHVER:=5.10/PATCHVER:=5.15/g' openwrt/target/linux/x86/Makefile
+# 修改内核 5.10改为5.15
+#sed -i 's/PATCHVER:=5.10/PATCHVER:=5.15/g' openwrt/target/linux/x86/Makefile
 
 # 修改默认主题
 sed -i 's/bootstrap/argon/' openwrt/feeds/luci/collections/luci/Makefile
